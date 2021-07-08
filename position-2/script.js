51de066adca415b5758236e1ef6115caecf253a1
leikimya <leikimiya@mail.ru>
2021-07-08 22:02:48 +0300

    js start

---
+++ position-2/script.js
@@ -0,0 +1,8 @@
+let hello = `Hello World!`
+var hello2 = `Hello World 2!`
+const hello3 = `Hello World 3!`
+// while(true)
+// alert(`Активируйте Уголь`)
+console.log (hello)
+console.log (hello2)
+console.log (hello3)
